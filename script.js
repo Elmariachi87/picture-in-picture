9744678114ef10b0a19263f1f6c6be4af33ea78c
Joe Hookham <joehookdev@gmail.com>
2022-03-17 20:29:41 +0000

    Finished core feature of JS file - pic in pic works

--- script.js
+++ script.js
@@ -1,13 +1,51 @@
-if ('pictureInPictureEnabled' in document) {
-    pipButton.classList.remove('hidden')
-    pipButton.disabled = false;
+// if ('pictureInPictureEnabled' in document) {
+//     pipButton.classList.remove('hidden')
+//     pipButton.disabled = false;
 
-    pipButton.addEventListener('click', () => {
-        video.requestPictureInPicture();
-    });
+//     pipButton.addEventListener('click', () => {
+//         video.requestPictureInPicture();
+//     });
+// }
+// // The Screen Capture API is relatively simple to use. Its sole method is MediaDevices.getDisplayMedia(), whose job is to ask the user to select a screen or portion of a screen to capture in the form of a MediaStream.
+
+// // To start capturing video from the screen, you call getDisplayMedia() on the instance of Media navigator.mediaDevices:
+
+// captureStream = await navigator.mediaDevices.getDisplayMedia(displayMediaOptions);
+
+
+const videoElement = document.getElementById('video');
+const button = document.getElementById('button');
+
+// Prompt user to select media stream, pass to video element, then play
+
+async function selectMediaStream() {
+    try {
+        // This variable is waiting for the user to state which screen they want to share. 'await' waits for this before declaring the variable.
+        const mediaStream = await navigator.mediaDevices.getDisplayMedia();
+        // Then we pass mediaStream (chosen media) to the source for the video player (i.e. a file to play)
+        videoElement.srcObject = mediaStream;
+        // When the video has loaded its metadata it will call a function which will play the video!
+        videoElement.onloadeddata = () => {
+            videoElement.play();
+        }
+    }
+    catch (error) {
+        //Catch error here
+        console.log('Ah beans, there\'s an error:', error);
+    }
 }
-// The Screen Capture API is relatively simple to use. Its sole method is MediaDevices.getDisplayMedia(), whose job is to ask the user to select a screen or portion of a screen to capture in the form of a MediaStream.
 
-// To start capturing video from the screen, you call getDisplayMedia() on the instance of Media navigator.mediaDevices:
+button.addEventListener('click', async () => {
+    // The line below was just me tinkering to see how I could add an attribute from JS
+    // document.getElementById("video").hidden = true;
 
-captureStream = await navigator.mediaDevices.getDisplayMedia(displayMediaOptions);
+    // Disable button
+    button.disabled = true;
+    // Start picture in picture
+    await videoElement.requestPictureInPicture();
+    // Reset button
+    button.disabled = false;
+});
+
+// On load
+selectMediaStream();
